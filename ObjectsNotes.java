83b0d9d4b27182f28799c8a2e659b601396b60ca
Brian Godinez <55292738+BrianGodinez@users.noreply.github.com>
2020-09-16 14:40:51 -0400

    Update ObjectsNotes.java

--- ObjectsNotes.java
+++ ObjectsNotes.java
@@ -1,4 +1,6 @@
-public class Student {
+package com.company;
+
+public class student {
   // attributes
   private String name;
   private double grade;
@@ -7,20 +9,20 @@ public class Student {
   // no argument contructor
   public Student() {
 
-  }
+            }
 
   // parameterized contructor
   public Student(String n, double g, boolean b) {
-    name = n;
-    grade = g;
-    behavior = b;
-  }
+                name = n;
+                grade = g;
+                behavior = b;
+            }
 
   // overload contructor
   public Student(String n, double g) {
-    name = n;
-    grade = g;
-  }
+                name = n;
+                grade = g;
+            }
 
   // methods
   public void changeGrade(double g) {
@@ -40,17 +42,16 @@ public class Student {
     return behavior;
   }
 
-public void printOutStudent()
-{
-  System.out.println("Name:\t" + getName() + "Gpa:\t" + getGrade() "Problem:\t" + getBehavior());
-}
+  public void printOutStudent() {
+    System.out.println("Name:\t" + getName() + "Gpa:\t" + getGrade() + "Problem:\t" + getBehavior());
+  }
 
   public static void main(String[] args) {
-    Student student1 = new Student("Brian", 6.9, false);
-    student1.printOutStudent();
+                Student student1 = new Student("Brian", 6.9, true);
+                student1.printOutStudent();
 
-    Student student2 = new Student("Briann", 9.6);
-    student1.printOutStudent(String n, double g);
+                Student student2 = new Student("Briann", 9.6);
+                student2.printOutStudent(String n, double g);
 
-  }
+            }
 }
